af7569881dc0d0a92378891e0014797810547e81
Karina Ionkina <kionkina@stuy.edu>
2018-02-28 18:39:05 -0500

    circle on mouse click

--- svg.js
+++ svg.js
@@ -6,11 +6,11 @@ var create_dot = function(e){
     console.log("running . . .");
     e.preventDefault();
     var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-
-    c1.setAttribute("cx", 200);
-    c1.setAttribute("cx", 300);
+    c1.setAttribute("fill", "red");
+    c1.setAttribute("cx", e.offsetX);
+    c1.setAttribute("cy", e.offsetY);
     c1.setAttribute("r", 10)
     pic.appendChild(c1);
 }
 
-beep.addEventListener("click", create_dot);
+pic.addEventListener("click", create_dot);
